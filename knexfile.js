119f60d667c5d88755fdf2940a1bb0e5f10802dd
tarmes <traarmes@gmail.com>
2020-12-09 15:48:17 -0500

    file structure

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+
+  },
+
+  production: {
+
+  }
+
+};
